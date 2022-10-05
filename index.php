3d983e271d2fab29ea7647c6579c63ece48c6883
LeydiMontufar <leydimontufar16@gmail.com>
2022-10-04 22:45:19 -0500

    google

--- index.php
+++ index.php
@@ -12,15 +12,12 @@
   
     <nav class="navbar navbar-expand-lg navbar-light  bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand text-white" href="#">GOOGLE</a>
+    <a class="navbar-brand text-white" href="/home">GOOGLE</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item dropdown">
-          <a class="nav-link active text-white" aria-current="page" href="/home">INICIO</a>
-        </li>
         <li class="nav-item dropdown">
           <a class="nav-link text-white" href="/CrearUsuario">REGISTRO</a>
         </li>
