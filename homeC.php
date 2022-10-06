0942d08f771b56d020c89e81c38c56341275d6a6
LeydiMontufar <leydimontufar16@gmail.com>
2022-10-06 13:46:12 -0500

    colacion

--- homeC.php
+++ homeC.php
@@ -1,5 +1,45 @@
 <?php include('index1.php'); ?>
 </div>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="fs-1">Muebles Mi Casa</h5>
+        <p class="fs-1">las tendencias desaparecen el estilo es eterno.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./images/4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="fs-1">MISIÓN:</h5>
+        <p class="fs-1">Venta de muebles nuevos y viejos para satisfacer los gustos y requerimientos de los clientes.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./images/4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="fs-1">VISIÓN:</h5>
+        <p class="fs-1">Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
 <head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
