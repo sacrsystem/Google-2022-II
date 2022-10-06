b4ec62a0dec64b37dcb78f23e0de89d4fff33b1f
Jaime supo <jaimesupo@gmail.com>
2022-10-06 09:57:12 -0500

    wer

--- handler.php
+++ handler.php
@@ -79,7 +79,15 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/cerrar':
         require 'cerrar.php';
         break;
-
+    case '/Buscar':
+            require 'buscar.php';
+            break;
+    case '/Catalogobuscar':
+            require 'Catalogobuscar.php';
+            break;
+    case '/hola':
+            require 'hola.php';
+            break;
     default:
         http_response_code(404);
         exit('Not Found');
