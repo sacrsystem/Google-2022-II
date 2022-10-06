fdf831d2d989b2a7f6d1d9171c58c2833fafb757
LeydiMontufar <leydimontufar16@gmail.com>
2022-10-05 19:52:26 -0500

    carrrrr

--- f_agregar_carrito.php
+++ f_agregar_carrito.php
@@ -3,15 +3,13 @@ $idProducto = urlencode($_REQUEST['idProducto']);
 session_start();
 $idUsuario =$_SESSION["idUsuario"];
 
-$url = "https://restaurantesofi.000webhostapp.com/serviciosMuebles/agregarProductoCarrito?idUsuario=".$idUsuario."&idProducto=".$idProducto;
+$url = "https://restaurantesofi.000webhostapp.com/serviciosMuebles/agregarProductoCarrito.php?idUsuario=".$idUsuario."&idProducto=".$idProducto;
 echo $url;
 $resultado = file_get_contents($url);
 
 if($resultado == "1"){
     header("Location: /CatalogoC");
     die();
-}else{
-    header("Location: /CatalogoC");
-    die();
 }
+
 ?>
