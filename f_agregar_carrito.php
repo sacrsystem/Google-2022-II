1310f6a6e0fee978f14b38065405d48824ab2f59
LeydiMontufar <leydimontufar16@gmail.com>
2022-10-16 20:16:53 -0500

    carrito

--- f_agregar_carrito.php
+++ f_agregar_carrito.php
@@ -5,7 +5,7 @@ $idUsuario =$_SESSION["idUsuario"];
 
 $url = "https://restaurantesofi.000webhostapp.com/serviciosMuebles/agregarProductoCarrito.php?idUsuario=".$idUsuario."&idProducto=".$idProducto;
 $resultado = file_get_contents($url);
-header("Location: /CatalogoC");
+header("Location: /CrearCarrito");
 
 /*if($resultado == "1"){
     header("Location: /CatalogoC");
