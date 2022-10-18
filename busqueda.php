32966aeb5ba7007fa72f41cedfde48b2982c565b
Jaime supo <jaimesupo@gmail.com>
2022-10-17 20:20:48 -0500

    bucar

--- busqueda.php
+++ busqueda.php
@@ -1,6 +1,6 @@
 
 <?php
-
+session_start();
 require_once './vendor/autoload.php';
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 use Google\Cloud\Vision\V1\Feature\Type;
@@ -12,9 +12,12 @@ $imageAnnotator = new ImageAnnotatorClient(
         'credentials' => 'key.json'
     ]
 );
-$path = "https://img.freepik.com/vector-gratis/conjunto-muebles-hogar_74855-15461.jpg?w=2000";
- $image = file_get_contents($path);
- //$image= fopen($_FILES['image']['tmp_name'],'r');
+//$path = "https://img.freepik.com/vector-gratis/conjunto-muebles-hogar_74855-15461.jpg?w=2000";
+ //$image = file_get_contents($path);
+ $image= fopen($_FILES['image']['tmp_name'],'r');
+ //$imagetoken=random_int(1111111,999999999);
+ //move_uploaded_file($_FILES['image']['tmp_name'],__DIR__.'/imagen/'.$imagetoken.'png');
+ //$imagen=$imageAnnotator->image($image,['FACE_DETECTION']['WEB_DETECTION']);
  $response = $imageAnnotator->objectLocalization($image);
  $objects = $response->getLocalizedObjectAnnotations();
 
@@ -27,3 +30,5 @@ $path = "https://img.freepik.com/vector-gratis/conjunto-muebles-hogar_74855-1546
      printf($name);
     
     print(PHP_EOL);}?>
+    
+
