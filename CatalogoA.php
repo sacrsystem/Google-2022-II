24a14996a69437e8f138624b670c461fe3d4d1e1
LeydiMontufar <leydimontufar16@gmail.com>
2022-10-19 14:54:59 -0500

    catalogoc

--- CatalogoA.php
+++ CatalogoA.php
@@ -4,62 +4,13 @@ $stringApi = "https://restaurantesofi.000webhostapp.com/serviciosMuebles/listaPr
 $data = json_decode(file_get_contents($stringApi),true);
 
 ?>
-
-    <div class="bg-secondary.bg-gradient p-5 rounded-5 text-secondary shadow mt-1" style="width: 90rem">
-      <div class="text-center fs-1 fw-bold">CATALOGO</div>
-      <div class="input-group mt-1">
-       <div> 
-       <div>
-
-           <div class="tab-pane fade show" id="pills-lunch" role="tabpanel" aria-labelledby="pills-lunch-tab" tabindex="0">
-                    <div class="row gy-3">
-
-
-                    <?php
-                    foreach ( $data as $items ) {
-                      $idProducto = $items['idProducto'];   
-                    ?>
-              
-                        <div class="col-lg-3 col-sm-5">
-                            <div class="menu-item bg-white shadow-on-hover">
-                            <div class="col">
-                                <div class="card h-100">
-                                  <img src="imagen/<?php echo $items['Imagen']; ?>" class="card-img-top" alt="...">
-                                  <div class="card-body">
-                                  <h5 class="card-title" ><?php echo $items['Producto']; ?></h5>
-                                    <p class="card-text"><?php echo $items['Descripcion'];?></p>
-                                    <p class="card-text"><?php echo $items['Precio'];?></p>
-                                    <p class="card-text"><?php echo $items['Stock'];?></p>
-                                    <a href="/EditarProducto?idProducto='<?php echo $idProducto;?>'" class="btn btn-primary">Editar</a>
-                                    <a href="/f_eliminar_producto?idProducto=<?php echo $idProducto;?>" class="btn btn-primary">Eliminar</a>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                        </div>
-                    
-                    <?php
-                      }
-                    ?>
-
-                   
-
-                    </div>
-                  </div>
-            </div>
-
-      </div>
-      </div>
-    </div>
-    </div>
-  </div>
-
   <link href="/estilos.css" rel="stylesheet">
     <!--Contenido-->
     <main>
     
         <div class="container">
         <div class="text-center fs-1 fw-bold">CATALOGO</div>
+        <br>
         <br>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <?php
@@ -78,9 +29,8 @@ $data = json_decode(file_get_contents($stringApi),true);
                           <div class="d-flex justify-content-between align-items-center">
                           <div class="btn-group">
                           <a href="/EditarProducto?idProducto='<?php echo $idProducto;?>'" class="btn btn-primary">Editar</a>
-                          <a href="/f_eliminar_producto?idProducto=<?php echo $idProducto;?>" class="btn btn-primary">Eliminar</a>
                           </div>
-                           
+                          <a href="/f_eliminar_producto?idProducto=<?php echo $idProducto;?>" class="btn btn-primary">Eliminar</a>
                         </div>
                         </div>    
                     </div>
