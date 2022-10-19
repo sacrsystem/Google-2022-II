68b1aeda42f99c3c232e67219532d4d9a3cef55a
Jaime supo <jaimesupo@gmail.com>
2022-10-18 23:19:49 -0500

    log

--- f_editar_producto.php
+++ f_editar_producto.php
@@ -4,12 +4,12 @@ $producto = urlencode("'".$_POST['producto']."'");
 $descripcion =urlencode("'".$_POST['descripcion']."'");
 $precio = urlencode("'".$_POST['precio']."'");
 $stock = urlencode("'".$_POST['stock']."'");
-$imagen =  urlencode("'".str_replace(" ","",$_POST['producto']).".".basename( $_FILES['imagen']['type'])."'");
+//$imagen =  urlencode("'".str_replace(" ","",$_POST['producto']).".".basename( $_FILES['imagen']['type'])."'");
 //str_replace(" ", "", $producto)
 $target_path = "imagen/";
 $target_path = $target_path.$_POST['producto'].".".basename( $_FILES['imagen']['type']);
 //restaurantesofi.000webhostapp.com/serviciosMuebles/agregarProducto.php?producto=%27Ropero%20de%20Melamine%27&descripcion=%27Marca%20PAZART%20MUEBLES%20-%20MADESA%27&precio=%27950%27&stock=%275%27&imagen=%27ropero.jpg%27
-$url = "https://restaurantesofi.000webhostapp.com/serviciosMuebles/editarProducto.php?idProducto=".$idProducto."&producto=".$producto."&descripcion=".$descripcion."&precio=".$precio."&stock=".$stock."&imagen=".$imagen;
+$url = "https://restaurantesofi.000webhostapp.com/serviciosMuebles/editarProducto.php?idProducto=".$idProducto."&producto=".$producto."&descripcion=".$descripcion."&precio=".$precio."&stock=".$stock;
 
 $resultado = file_get_contents($url);
 
